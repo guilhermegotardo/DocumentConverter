8a05335b3ee95b63429d3db7fecd89b7eb8d59c9
Guilherme Gotardo <guilhermegotardo@outlook.com>
2020-12-15 08:57:06 -0300

    source file

---
+++ scr/DocumentConverter.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+df = pd.read_excel (r'C:\Users\ACER\D2\Dev\011\Modelo.xlsx')
+print (df)
