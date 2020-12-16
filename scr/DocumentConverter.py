8d7a9396b2c750d120494e5606031ec0af9612b5
Guilherme Gotardo <guilhermegotardo@outlook.com>
2020-12-15 23:29:46 -0300

    finding all lines

--- scr/DocumentConverter.py
+++ scr/DocumentConverter.py
@@ -5,7 +5,10 @@ Main File to convert bills
 
 import pandas as pd
 df = pd.read_excel (r'C:\Users\ACER\D2\Dev\011\Modelo.xlsx')
+print(df)
 CurrentLine= (df.iat[0, 0] )
-DChar = CurrentLine.find("D", (len(CurrentLine)-5), len(CurrentLine))
-print (DChar)
+for i in range(0, len(df)):
+    CurrentLine= (df.iat[i, 0] )
+    DChar = CurrentLine.find("D", (len(CurrentLine)-5), len(CurrentLine))
+    print (DChar)
 
