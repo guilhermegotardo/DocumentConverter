7dbb0701c6d1a52a9d18618aa63c28e24545e855
Guilherme Gotardo <guilhermegotardo@outlook.com>
2020-12-15 23:18:33 -0300

    finding index of D

--- scr/DocumentConverter.py
+++ scr/DocumentConverter.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+Main File to convert bills 
 """
 
 import pandas as pd
 df = pd.read_excel (r'C:\Users\ACER\D2\Dev\011\Modelo.xlsx')
-print (df)
+CurrentLine= (df.iat[0, 0] )
+DChar = CurrentLine.find("D", (len(CurrentLine)-5), len(CurrentLine))
+print (DChar)
+
