d606a193433b742701b4a34f9ccb734ae128f417
Guilherme Gotardo <guilhermegotardo@outlook.com>
2020-12-22 13:53:59 -0300

    finding specific vendor

--- scr/DocumentConverter.py
+++ scr/DocumentConverter.py
@@ -4,11 +4,22 @@ Main File to convert bills
 """
 
 import pandas as pd
+import numpy as np
+
+ValueArray = np.zeros(50)
+
 df = pd.read_excel (r'C:\Users\ACER\D2\Dev\011\Modelo.xlsx')
 print(df)
 CurrentLine= (df.iat[0, 0] )
+
 for i in range(0, len(df)):
     CurrentLine= (df.iat[i, 0] )
-    DChar = CurrentLine.find("D", (len(CurrentLine)-5), len(CurrentLine))
-    print (DChar)
+    PosLine = CurrentLine.find("D", (len(CurrentLine)-7), len(CurrentLine))
+    if PosLine != -1  and (CurrentLine.find("UBER") or CurrentLine.find("UBERBR")  ):
+        NewLine = CurrentLine.replace(',', '.')
+        ValueArray[i] =  (NewLine[(len(NewLine)-7):(len(NewLine)-2)])
+        print (ValueArray[i])
+       
+        
+        
 
